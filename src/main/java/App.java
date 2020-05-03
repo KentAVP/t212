0573edc03a108f7c7f0c9ff51a8d7fbb7601987d
KentAVP <60615041+KentAVP@users.noreply.github.com>
2020-05-03 18:35:25 +0700

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = 
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        boolean checkHello = bean==bean2;
+        System.out.println(checkHello);
+        Cat cat1 = (Cat)applicationContext.getBean("cat");
+        Cat cat2 = (Cat)applicationContext.getBean("cat");
+        boolean checkcat = cat1==cat2;
+        System.out.println(checkcat);
+    }
+}
